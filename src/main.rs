3a4ffce363c6e4c7359d8887db1562244217b690
Kodi Craft <kodi@kdcf.me>
2022-11-24 23:44:00 +0100

    Started setting up project

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod xrs;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/xrs/requests/types.rs
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Describe the request types
+
+
+//use xrb
+
+
+// Each request type is a struct with visible fields, each request 
+// type has a corresponding request type in the xrb module, as
+// well as a bunch of fields that we want to abstract away
+// from the user.
+
